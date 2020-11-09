77d15798e91fae581c0f8c7a62f0715dd7e7911c
jsm-txt <hose153@gmail.com>
2020-11-09 15:37:01 -0600

    print reciept function added
    prints users information

--- BankAccount.py
+++ BankAccount.py
@@ -13,22 +13,34 @@ class BankAccount:
 
     def withdraw(self, amount):
         if amount > self._balance:
+            self._balance = self._balance - 10
             print('Insufficient funds')
         else:
             self._balance = self._balance - amount
             return print(f'Amount Withdrawn: ${amount}')
 
-    def get_balance():
-        pass
+    def get_balance(self):
+        balance = self._balance
+        return print(f'Current Balance ${"{:.2f}".format(balance)}')
 
-    def add_interest():
-        pass
-
-    def print_receipt():
-        pass
+    def add_interest(self):
+        interest = self._balance *  0.00083
+        self._balance = self._balance +interest
+        return 
+    def print_receipt(self):
+        account = 0
+        print(f'{self._name}')
+        print(f' Account No.: {account}')
+        print(f'Routing No.: {self._routing_number}')
+        print(f'Balance: ${"{:.2f}".format(self._balance)}')
+        return 
 
 
 jj = BankAccount('JJ',100000000,123456789, 200)
 jj.deposit(200)
 jj.withdraw(500)
 jj.withdraw(100)
+jj.get_balance()
+jj.add_interest()
+jj.get_balance()
+jj.print_receipt()
