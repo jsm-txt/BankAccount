1c0d30266bf6da0d4c9efb81902f9b9b1eea8904
jsm-txt <hose153@gmail.com>
2020-11-10 14:43:26 -0600

    finished some of the mistakes
    bank account small issues fixed

--- BankAccount.py
+++ BankAccount.py
@@ -1,11 +1,12 @@
+from random import randint
+
 class BankAccount:
+    routing_number = 123456789
 
-    def __init__(self, name, account_number, routing_number, balance):
+    def __init__(self, name):
         self._name = name
-        self._account_number = account_number
-        self._routing_number = routing_number 
-        self._balance = balance
-
+        self._account_number = randint(10000000,19999999)
+        self._balance = 0
 
     def deposit(self, amount):
         self._balance = amount + self._balance
@@ -30,16 +31,16 @@ class BankAccount:
     def print_receipt(self):
         account = self._account_number
         account = account % 10000
-
+        routing_number =0
 
         print(f'{self._name}')
         print(f'Account No.: ****{account}')
-        print(f'Routing No.: {self._routing_number}')
+        print(f'Routing No.: {BankAccount.routing_number}')
         print(f'Balance: ${"{:.2f}".format(self._balance)}')
         return 
 
 
-jj = BankAccount('JJ',13032340,123456789, 200)
+jj = BankAccount('JJ')
 jj.deposit(200)
 jj.withdraw(500)
 jj.withdraw(100)
@@ -48,17 +49,17 @@ jj.add_interest()
 jj.get_balance()
 jj.print_receipt()
 
-aa = BankAccount( 'Aa', 10232343, 123456789, 1000)
+aa = BankAccount( 'Aa')
 aa.deposit(300)
 aa.withdraw(1300)
 aa.get_balance()
 aa.add_interest()
 aa.print_receipt()
 
-dd = BankAccount('DD', 10478372, 123456789, 0)
-aa.deposit(500)
-aa.withdraw(1000)
-aa.withdraw(100)
-aa.get_balance()
-aa.add_interest()
-aa.print_receipt()
+dd = BankAccount('DD')
+dd.deposit(500)
+dd.withdraw(1000)
+dd.withdraw(100)
+dd.get_balance()
+dd.add_interest()
+dd.print_receipt()
